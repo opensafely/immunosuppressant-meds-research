1b72fe1875633e6323a9bd0ebd6fb5659672ad85
Nick Kennedy <nick.kennedy1@nhs.net>
2020-09-23 01:42:32 +0100

    First proper go at study_definition.py

---
+++ analysis/codelists.py
@@ -0,0 +1,187 @@
+from cohortextractor import (
+    codelist_from_csv,
+    codelist,
+)
+
+
+# OUTCOME CODELISTS
+covid_identification = codelist_from_csv(
+    "codelists/opensafely-covid-identification.csv",
+    system="icd10",
+    column="icd10_code",
+)
+
+covid_pos_primcare_code = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-clinical-code.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+covid_pos_primcare_test = codelist_from_csv(
+    "codelists/opensafely-covid-identification-in-primary-care-probable-covid-positive-test.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+
+# MEDICATIONS - EXPOSURES
+
+
+# DEMOGRAPHIC CODELIST
+ethnicity_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
+
+# SMOKING CODELIST
+clear_smoking_codes = codelist_from_csv(
+    "codelists/opensafely-smoking-clear.csv",
+    system="ctv3",
+    column="CTV3Code",
+    category_column="Category",
+)
+
+unclear_smoking_codes = codelist_from_csv(
+    "codelists/opensafely-smoking-unclear.csv",
+    system="ctv3",
+    column="CTV3Code",
+    category_column="Category",
+)
+
+# CLINICAL CONDITIONS CODELISTS
+crohns_disease_codes = codelist_from_csv(
+    "codelists/crossimid-crohns-disease.csv", system="ctv3", column="CTV3ID",
+)
+
+ulcerative_colitis_codes = codelist_from_csv(
+    "codelists/crossimid-ulcerative-colitis.csv", system="ctv3", column="CTV3ID",
+)
+
+inflammatory_bowel_disease_unclassified_codes = codelist_from_csv(
+    "codelists/crossimid-inflammatory-bowel-disease-unclassified.csv", system="ctv3", column="CTV3ID",
+)
+
+psoriasis_codes = codelist_from_csv(
+    "codelists/crossimid-psoriasis.csv", system="ctv3", column="CTV3ID",
+)
+
+psoriatic_arthritis_codes = codelist_from_csv(
+    "codelists/crossimid-psoriatic-arthritis.csv", system="ctv3", column="CTV3ID",
+)
+
+rheumatoid_arthritis_codes = codelist_from_csv(
+    "codelists/opensafely-rheumatoid-arthritis.csv", system="ctv3", column="CTV3ID",
+)
+
+chronic_cardiac_disease_codes = codelist_from_csv(
+    "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID",
+)
+
+diabetes_codes = codelist_from_csv(
+    "codelists/opensafely-diabetes.csv", system="ctv3", column="CTV3ID",
+)
+
+hba1c_new_codes = codelist(["XaPbt", "Xaeze", "Xaezd"], system="ctv3")
+hba1c_old_codes = codelist(["X772q", "XaERo", "XaERp"], system="ctv3")
+
+hypertension_codes = codelist_from_csv(
+    "codelists/opensafely-hypertension.csv", system="ctv3", column="CTV3ID",
+)
+
+chronic_respiratory_disease_codes = codelist_from_csv(
+    "codelists/opensafely-chronic-respiratory-disease.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+copd_codes = codelist_from_csv(
+    "codelists/opensafely-current-copd.csv", system="ctv3", column="CTV3ID",
+)
+
+chronic_liver_disease_codes = codelist_from_csv(
+    "codelists/opensafely-chronic-liver-disease.csv", system="ctv3", column="CTV3ID",
+)
+
+current_asthma_codes = codelist_from_csv(
+    "codelists/opensafely-current-asthma.csv", system="ctv3", column="CTV3ID",
+)
+
+stroke_codes = codelist_from_csv(
+    "codelists/opensafely-stroke-updated.csv", system="ctv3", column="CTV3ID",
+)
+
+lung_cancer_codes = codelist_from_csv(
+    "codelists/opensafely-lung-cancer.csv", system="ctv3", column="CTV3ID",
+)
+
+haem_cancer_codes = codelist_from_csv(
+    "codelists/opensafely-haematological-cancer.csv", system="ctv3", column="CTV3ID",
+)
+
+other_cancer_codes = codelist_from_csv(
+    "codelists/opensafely-cancer-excluding-lung-and-haematological.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+creatinine_codes = codelist(["XE2q5"], system="ctv3")
+
+ckd_codes = codelist_from_csv(
+    "codelists/opensafely-chronic-kidney-disease.csv", system="ctv3", column="CTV3ID",
+)
+
+organ_transplant_codes = codelist_from_csv(
+    "codelists/opensafely-solid-organ-transplantation.csv",
+    system="ctv3",
+    column="CTV3ID",
+)
+
+# Medication
+
+oral_pred_codes = codelist_from_csv(
+    "codelists/opensafely-asthma-oral-prednisolone-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
+anti_tnf_med_codes = codelist_from_csv(
+    "codelists/crossimid-anti-tnf-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
+anti_il6_med_codes = codelist_from_csv(
+    "codelists/crossimid-anti-il6-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
+anti_il12_23_med_codes = codelist_from_csv(
+    "codelists/crossimid-anti-il12-23-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
+anti_il1_med_codes = codelist_from_csv(
+    "codelists/crossimid-anti-il1-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
+anti_il4_med_codes = codelist_from_csv(
+    "codelists/crossimid-anti-il4-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
+jak_inhibitors_med_codes = codelist_from_csv(
+    "codelists/crossimid-jak-inhibitors-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
+standard_systemic_immunosuppressant_med_codes = codelist_from_csv(
+    "codelists/opensafely-dmards.csv",
+    system="snomed",
+    column="snomed_id",
+)
+rituximab_med_codes = codelist_from_csv(
+    "codelists/crossimid-rituximab-medication.csv",
+    system="snomed",
+    column="snomed_id",
+)
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,19 +1,5 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv
 
-chronic_cardiac_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-cardiac-disease.csv", system="ctv3", column="CTV3ID"
-)
-chronic_liver_disease_codes = codelist_from_csv(
-    "codelists/opensafely-chronic-liver-disease.csv", system="ctv3", column="CTV3ID"
-)
-salbutamol_codes = codelist_from_csv(
-    "codelists/opensafely-asthma-inhaler-salbutamol-medication.csv",
-    system="snomed",
-    column="id",
-)
-systolic_blood_pressure_codes = codelist(["2469."], system="ctv3")
-diastolic_blood_pressure_codes = codelist(["246A."], system="ctv3")
-
 study = StudyDefinition(
     # Configure the expectations framework
     default_expectations={
@@ -22,12 +8,42 @@ study = StudyDefinition(
     },
     # This line defines the study population
     population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+        "2019-03-01", "2020-03-01"
+    ),
+    # Outcomes
+    icu_date_admitted=patients.admitted_to_icu(
+        on_or_after="2020-03-01",
+        include_day=True,
+        returning="date_admitted",
+        find_first_match_in_period=True,
+        return_expectations={"date": {"earliest": "2020-03-01"}},
+    ),
+    died_date_cpns=patients.with_death_recorded_in_cpns(
+        on_or_before="2020-06-01",
+        returning="date_of_death",
+        include_month=True,
+        include_day=True,
+        return_expectations={"date": {"earliest": "2020-03-01"}},
+    ),
+    died_ons_covid_flag_any=patients.with_these_codes_on_death_certificate(
+        covid_codelist, on_or_after="2020-03-01", match_only_underlying_cause=False,
+        return_expectations={"date": {"earliest": "2020-03-01"}},
+    ),
+    died_ons_covid_flag_underlying=patients.with_these_codes_on_death_certificate(
+        covid_codelist, on_or_after="2020-03-01", match_only_underlying_cause=True,
+        return_expectations={"date": {"earliest": "2020-03-01"}},
+    ),
+    died_date_ons=patients.died_from_any_cause(
+        on_or_after="2020-06-01",
+        returning="date_of_death",
+        include_month=True,
+        include_day=True,
+        return_expectations={"date": {"earliest": "2020-03-01"}},
     ),
     # The rest of the lines define the covariates with associated GitHub issues
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/33
     age=patients.age_as_of(
-        "2020-02-01",
+        "2020-03-01",
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
@@ -40,16 +56,138 @@ study = StudyDefinition(
             "category": {"ratios": {"M": 0.49, "F": 0.51}},
         }
     ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/7
-    chronic_cardiac_disease=patients.with_these_clinical_events(
+    # Disease codes
+    crohns_disease=patients.with_these_clinical_events
+        crohns_disease_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    ulcerative_colitis=patients.with_these_clinical_events
+        ulcerative_colitis_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    inflammatory_bowel_disease_unclassified=patients.with_these_clinical_events
+        inflammatory_bowel_disease_unclassified_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    psoriasis=patients.with_these_clinical_events
+        psoriasis_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    psoriatic_arthritis=patients.with_these_clinical_events
+        psoriatic_arthritis_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    rheumatoid_arthritis=patients.with_these_clinical_events
+        rheumatoid_arthritis_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    chronic_cardiac_disease=patients.with_these_clinical_events
         chronic_cardiac_disease_codes,
         returning="date",
         find_first_match_in_period=True,
         include_month=True,
-        return_expectations={"incidence": 0.2},
-    ),
-    # https://github.com/ebmdatalab/tpp-sql-notebook/issues/12
-    chronic_liver_disease=patients.with_these_clinical_events(
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    diabetes=patients.with_these_clinical_events
+        diabetes_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    hba1c_new=patients.with_these_clinical_events
+        hba1c_new_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    hba1c_old=patients.with_these_clinical_events
+        hba1c_old_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    hypertension=patients.with_these_clinical_events
+        hypertension_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    chronic_respiratory_disease=patients.with_these_clinical_events
+        chronic_respiratory_disease_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    copd=patients.with_these_clinical_events
+        copd_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    chronic_liver_disease=patients.with_these_clinical_events
         chronic_liver_disease_codes,
         returning="date",
         find_first_match_in_period=True,
@@ -58,7 +196,87 @@ study = StudyDefinition(
             "incidence": 0.2,
             "date": {"earliest": "1950-01-01", "latest": "today"},
         },
-    ),
+    ,
+    current_asthma=patients.with_these_clinical_events
+        current_asthma_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    stroke=patients.with_these_clinical_events
+        stroke_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    lung_cancer=patients.with_these_clinical_events
+        lung_cancer_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    haem_cancer=patients.with_these_clinical_events
+        haem_cancer_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    other_cancer=patients.with_these_clinical_events
+        other_cancer_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    creatinine=patients.with_these_clinical_events
+        creatinine_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    ckd=patients.with_these_clinical_events
+        ckd_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
+    organ_transplant=patients.with_these_clinical_events
+        organ_transplant_codes,
+        returning="date",
+        find_first_match_in_period=True,
+        include_month=True,
+        return_expectations={
+            "incidence": 0.2,
+            "date": {"earliest": "1950-01-01", "latest": "today"},
+        },
+    ,
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/10
     bmi=patients.most_recent_bmi(
         on_or_after="2010-02-01",
@@ -74,7 +292,7 @@ study = StudyDefinition(
     bp_sys=patients.mean_recorded_value(
         systolic_blood_pressure_codes,
         on_most_recent_day_of_measurement=True,
-        on_or_before="2020-02-01",
+        on_or_before="2020-03-01",
         include_measurement_date=True,
         include_month=True,
         return_expectations={
@@ -85,7 +303,7 @@ study = StudyDefinition(
     bp_dias=patients.mean_recorded_value(
         diastolic_blood_pressure_codes,
         on_most_recent_day_of_measurement=True,
-        on_or_before="2020-02-01",
+        on_or_before="2020-03-01",
         include_measurement_date=True,
         include_month=True,
         return_expectations={
@@ -95,7 +313,7 @@ study = StudyDefinition(
     ),
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/54
     stp=patients.registered_practice_as_of(
-        "2020-02-01",
+        "2020-03-01",
         returning="stp_code",
         return_expectations={
             "rate": "universal",
@@ -103,7 +321,7 @@ study = StudyDefinition(
         },
     ),
     msoa=patients.registered_practice_as_of(
-        "2020-02-01",
+        "2020-03-01",
         returning="msoa_code",
         return_expectations={
             "rate": "universal",
@@ -112,7 +330,7 @@ study = StudyDefinition(
     ),
     # https://github.com/ebmdatalab/tpp-sql-notebook/issues/52
     imd=patients.address_as_of(
-        "2020-02-01",
+        "2020-03-01",
         returning="index_of_multiple_deprivation",
         round_to_nearest=100,
         return_expectations={
@@ -121,7 +339,7 @@ study = StudyDefinition(
         },
     ),
     rural_urban=patients.address_as_of(
-        "2020-02-01",
+        "2020-03-01",
         returning="rural_urban_classification",
         return_expectations={
             "rate": "universal",
@@ -130,11 +348,126 @@ study = StudyDefinition(
     ),
     recent_salbutamol_count=patients.with_these_medications(
         salbutamol_codes,
-        between=["2018-02-01", "2020-02-01"],
+        between=["2018-02-01", "2020-03-01"],
         returning="number_of_matches_in_period",
         return_expectations={
             "incidence": 0.6,
             "int": {"distribution": "normal", "mean": 8, "stddev": 2},
         },
     ),
+    #SMOKING
+    smoking_status=patients.categorised_as(
+        {
+            "S": "most_recent_smoking_code = 'S'",
+            "E": """
+                     most_recent_smoking_code = 'E' OR (    
+                       most_recent_smoking_code = 'N' AND ever_smoked   
+                     )  
+                """,
+            "N": "most_recent_smoking_code = 'N' AND NOT ever_smoked",
+            "M": "DEFAULT",
+        },
+        return_expectations={
+            "category": {"ratios": {"S": 0.6, "E": 0.1, "N": 0.2, "M": 0.1}}
+        },
+        most_recent_smoking_code=patients.with_these_clinical_events(
+            clear_smoking_codes,
+            find_last_match_in_period=True,
+            on_or_before="2020-02-29",
+            returning="category",
+        ),
+        ever_smoked=patients.with_these_clinical_events(
+            filter_codes_by_category(clear_smoking_codes, include=["S", "E"]),
+            on_or_before="2020-02-29",
+        ),
+    ),
+    smoking_status_date=patients.with_these_clinical_events(
+        clear_smoking_codes,
+        on_or_before="2020-02-29",
+        return_last_date_in_period=True,
+        include_month=True,
+        return_expectations={"date": {"latest": "2020-02-29"}},
+    ),
+    # Medications
+    oral_prednisolone_count=patients.with_these_medications(
+        oral_pred_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ),
+    anti_tnf_med_count=patients.with_these_medications
+        anti_tnf_med_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ,
+    anti_il6_med_count=patients.with_these_medications
+        anti_il6_med_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ,
+    anti_il12_23_med_count=patients.with_these_medications
+        anti_il12_23_med_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ,
+    anti_il1_med_count=patients.with_these_medications
+        anti_il1_med_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ,
+    anti_il4_med_count=patients.with_these_medications
+        anti_il4_med_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ,
+    jak_inhibitors_med_count=patients.with_these_medications
+        jak_inhibitors_med_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ,
+    standard_systemic_immunosuppressant_med_count=patients.with_these_medications
+        standard_systemic_immunosuppressant_med_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ,
+    rituximab_med_count=patients.with_these_medications
+        rituximab_med_codes,
+        between=["2019-09-01", "2020-02-29"],
+        returning="number_of_matches_in_period",
+        return_expectations={
+            "int": {"distribution": "normal", "mean": 3, "stddev": 2},
+            "incidence": 0.1,
+        },
+    ,    
 )
