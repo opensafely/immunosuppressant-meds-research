419bbf9d94306a15ef347ff5fc184f6f8e41c179
Nick Kennedy <nick@nick-kennedy.com>
2021-08-07 00:27:24 +0100

    Add merge for CSVs from spline models

--- analysis/r-immunosuppressant-meds-research/make_forest_plots_with_3_models.R
+++ analysis/r-immunosuppressant-meds-research/make_forest_plots_with_3_models.R
@@ -19,13 +19,24 @@ if (!(basename(getwd()) %in% c("workspace", "immunosuppressant-meds-research")))
   cat("Working directory:", getwd())
   stop("Folder structure seems wrong")
 }
-csv_files <- list.files("output/data", pattern = "^csv_.*\\.csv", full.names = TRUE)
+csv_files <- list.files("output/data", pattern = "^csv_.*\\.csv", full.names = TRUE) %>%
+  str_subset("spline", negate = TRUE)
 model_outputs <- map_dfr(csv_files, read_csv) %>% 
   mutate(
     events_exposed = if_else(between(events_exposed, 1, 5), NA_real_, events_exposed),
     rate_exposed = if_else(is.na(events_exposed), NA_real_, rate_exposed),
   )
 write_csv(model_outputs, "output/data/merged_csv_normal.csv")
+
+csv_files_ho <- list.files("output/data", pattern = "^csvhaemonc_.*\\.csv", full.names = TRUE) %>%
+  str_subset("spline", negate = TRUE)
+model_outputs_ho <- map_dfr(csv_files_ho, read_csv) %>% 
+  mutate(
+    events_exposed = if_else(between(events_exposed, 1, 5), NA_real_, events_exposed),
+    rate_exposed = if_else(is.na(events_exposed), NA_real_, rate_exposed),
+  )
+write_csv(model_outputs_ho, "output/data/merged_csv_haemonc.csv")
+
 
 imr_fplot <- function(
   data,
